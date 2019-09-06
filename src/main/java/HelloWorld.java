68523aa3490e29f60e63c55b5df0312da0b165d8
lengochuyen25 <huyenln.hvnh@gmail.com>
2019-09-06 07:53:40 +0700

    a

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("A");
+    }
+
+    public static void main() {
+        System.out.println("B");    }
+
+    public static void start(String[] args) {
+        System.out.println("C");
+    }
+}
